855d5e24e1571217e7744e979e909fb45752c16e
Saicharukesh <78635753+Saicharukesh@users.noreply.github.com>
2021-06-14 12:23:22 +0530

    Add files via upload

---
+++ PRO-c24-template2-main/sketch.js
@@ -0,0 +1,37 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var hammer;
+var rubber;
+var stone;
+var iron;
+
+function setup(){
+    var canvas = createCanvas(1200,600);
+    engine = Engine.create();
+    world = engine.world;
+
+    plane = new Plane(600,height,1200,20);
+    hammer = new Hammer(10,100);
+
+    rubber= new Rubber (300,500,100/2);
+    stone = new Stone(200,190,30,50);
+
+    iron = new Iron(600,300,200,90);
+
+}
+
+function draw(){
+    background("lightBlue");
+    Engine.update(engine);
+
+
+    plane.display();
+    hammer.display();
+    rubber.display();
+    stone.display();
+    iron.display();
+ 
+}
